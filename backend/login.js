dd318db222d6fb64aa25e890e741b7b679fa1775
Kunal Sonne <153419118+erenyeager101@users.noreply.github.com>
2024-12-22 12:57:17 +0530

    handled env variables

--- backend/login.js
+++ backend/login.js
@@ -1,10 +1,11 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const { MongoClient } = require('mongodb'); 
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const helmet = require('helmet');
 
 const app = express();
 const port = 3001;
@@ -12,13 +13,14 @@ const port = 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(helmet());
 
-const uri = "mongodb+srv://kunalsonne:kunalsonne1847724@cluster0.95mdg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
 
+let collection;
 
-
-mongoose.connect('mongodb+srv://kunalsonne:kunalsonne1847724@cluster0.95mdg.mongodb.net/Auth', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
@@ -34,20 +36,19 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-
 async function connectToDatabase() {
     try {
         await client.connect();
-        console.log('MongoDB connected successfully ');
+        console.log('MongoDB connected successfully');
         const db = client.db('home');
         collection = db.collection('blogs');
     } catch (err) {
-        console.error('Failed to connect to MongoDB ', err);
-        process.exit(1); 
+        console.error('Failed to connect to MongoDB:', err.message);
+        process.exit(1);
     }
 }
 
-app.use(express.static('frontend'));
+app.use(express.static(path.join(__dirname, 'frontend')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
@@ -58,13 +59,11 @@ app.get('/home', (req, res) => {
 
 app.post('/signup', async (req, res) => {
     const { email, password } = req.body;
-
-    const newUser = new User({
-        email,
-        password 
-    });
+    const saltRounds = 10;
 
     try {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const newUser = new User({ email, password: hashedPassword });
         await newUser.save();
         res.status(201).send('User registered successfully');
     } catch (error) {
@@ -79,8 +78,8 @@ app.post('/signin', async (req, res) => {
         const user = await User.findOne({ email });
         if (!user) return res.status(404).send('User not found');
 
-        if (password === user.password) {
-            
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (isPasswordValid) {
             res.redirect('/home');
         } else {
             res.status(401).send('Invalid password');
